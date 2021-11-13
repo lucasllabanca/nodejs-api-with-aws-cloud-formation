1f342e9c67806202e65241d408e489806634e754
Lucas Labanca <lucas.labanca@fitcard.com.br>
2021-11-13 08:50:52 -0300

    Initial commit

---
+++ test/e_commerce_pos.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as ECommercePos from '../lib/e_commerce_pos-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/e_commerce_pos-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ECommercePos.ECommercePosStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
